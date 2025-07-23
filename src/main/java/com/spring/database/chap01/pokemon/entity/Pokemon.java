4d67d78a5f1a31eb7ce3d54f6bfe50d5c6ba730f
Kanggwanju <xprtktm1@gmail.com>
2025-07-23 13:00:04 +0900

    Pokemon 엔티티 및 PokemonRepository CRUD 구현, 테스트 코드 추가

---
+++ src/main/java/com/spring/database/chap01/pokemon/entity/Pokemon.java
@@ -0,0 +1,34 @@
+package com.spring.database.chap01.pokemon.entity;
+
+import lombok.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pokemon {
+    private Long id;
+    private String name;
+    private String type1;
+    private int hp;
+    private int attack;
+    private int defense;
+    private int speed;
+
+    public Pokemon(ResultSet rs) throws SQLException {
+        this.id = rs.getLong("id");
+        this.name = rs.getString("name");
+        this.type1 = rs.getString("type1");
+        this.hp = rs.getInt("hp");
+        this.attack = rs.getInt("attack");
+        this.defense = rs.getInt("defense");
+        this.speed = rs.getInt("speed");
+    }
+}
---
+++ src/test/java/com/spring/database/chap01/pokemon/entity/repository/PokemonRepositoryTest.java
@@ -0,0 +1,118 @@
+package com.spring.database.chap01.pokemon.entity.repository;
+
+import com.spring.database.chap01.entity.Book;
+import com.spring.database.chap01.pokemon.entity.Pokemon;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PokemonRepositoryTest {
+    
+    @Autowired
+    PokemonRepository pokemonRepository;
+    
+    @Test
+    @DisplayName("포켓몬 정보를 주면 데이터베이스 pokemon 테이블에 저장된다.")
+    void saveTest() {
+        //given
+        Pokemon givenPokemon = Pokemon.builder()
+                .name("파이리")
+                .type1("불")
+                .hp(300)
+                .attack(35)
+                .defense(15)
+                .speed(30)
+                .build();
+        //when
+        boolean flag = pokemonRepository.save(givenPokemon);
+
+        //then
+        Assertions.assertTrue(flag);
+    }
+    
+    @Test
+    @DisplayName("포켓몬의 체력, 공격력, 방어력, 속도를 수정해야 한다.")
+    void updateTest() {
+        //given
+        Pokemon updatedPokemon = Pokemon.builder()
+                .hp(250)
+                .attack(45)
+                .defense(40)
+                .speed(50)
+                .id(2L)
+                .build();
+        //when
+        boolean flag = pokemonRepository.updatePokemonStatus(updatedPokemon);
+        //then
+        assertTrue(flag);
+    }
+
+    @Test
+    @DisplayName("id를 주면 pokemon 테이블에서 해당 id를 가진 행이 삭제된다.")
+    void deleteTest() {
+        //given
+        Long givenId = 2L;
+        //when
+        boolean flag = pokemonRepository.deleteById(givenId);
+        //then
+        assertTrue(flag);
+    }
+    
+    @Test
+    @DisplayName("전체 조회를 하면 포켓몬의 리스트가 반환된다.")
+    void findAllTest() {
+        //given
+
+        //when
+        List<Pokemon> pokemonList = pokemonRepository.findAll();
+        //then
+        pokemonList.forEach(System.out::println);
+
+        assertEquals(4, pokemonList.size());
+        assertNotNull(pokemonList.get(0));
+        assertEquals("전기", pokemonList.get(0).getType1());
+    }
+    
+    @Test
+    @DisplayName("적합한 id를 통해 개별조회를 하면 포켓몬 1개의 객체가 반환된다.")
+    void findOneTest() {
+        //given
+        Long givenId = 4L;
+
+        //when
+        Pokemon foundPokemon = pokemonRepository.findById(givenId);
+
+        //then
+        System.out.println("foundPokemon = " + foundPokemon);
+
+        assertNotNull(foundPokemon);
+        assertEquals("롱스톤", foundPokemon.getName());
+    }
+
+    @Test
+    @DisplayName("공격하는 포켓몬의 공격력과 방어하는 포켓몬의 방어력을 비교하여 차이만큼 방어하는 포켓몬의 체력이 작아진다.")
+    void attackTest() {
+        //given
+        Pokemon attackingPokemon = pokemonRepository.findById(4L);
+        Pokemon defendingPokemon = pokemonRepository.findById(3L);
+
+        //when
+        boolean flag = pokemonRepository.pokemonAttack(attackingPokemon, defendingPokemon);
+
+        //then
+        assertTrue(flag);
+    }
+
+    
+    
+    
+
+
+}
